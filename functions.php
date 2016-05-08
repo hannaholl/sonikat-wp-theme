8e1b3df836843cb0bdcaad793e4ad8e76ef66ef7
hannaholl <hanna.hollstrom@gmail.com>
2016-05-08 20:08:32 +0100

    Style welcome and update pages

--- functions.php
+++ functions.php
@@ -9,7 +9,9 @@ function theme_enqueue_styles() {
         array( $parent_style )
     );
 
-    wp_enqueue_style('sonikat', get_stylesheet_directory_uri() . '/css/sonikat-header.css');
+    wp_enqueue_style('sonikat-header', get_stylesheet_directory_uri() . '/css/sonikat-header.css');
+    wp_enqueue_style('sonikat-welcome', get_stylesheet_directory_uri() . '/css/page-welcome.css');
+    wp_enqueue_style('sonikat-posts', get_stylesheet_directory_uri() . '/css/sonikat-posts.css');
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
--- page-welcome.php
+++ page-welcome.php
@@ -3,7 +3,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main page-welcome" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page-welcome' ); ?>

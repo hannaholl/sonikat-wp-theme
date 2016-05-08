5e99e217c16712dfb524aa48254020b6c630f185
hannaholl <hanna.hollstrom@gmail.com>
2016-05-08 14:17:21 +0100

    style nav

--- header.php
+++ header.php
@@ -42,9 +42,43 @@
 	do_action( 'photo_perfect_action_before' );
 	?>
 
+		<div class="header-img-nav">
+			<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+			<!-- NAV -->
+		  <nav id="site-navigation" class="header-navigation" role="navigation">
+          <?php
+            wp_nav_menu(
+              array(
+                'theme_location' => 'primary',
+                'menu_id'        => 'primary-menu',
+              )
+            );
+          ?>
+      </nav><!-- #site-navigation -->
+	    </div> <!-- #main-nav -->
+			<!-- NAV END -->
+		</div>
 
+		<?php $show_title = photo_perfect_get_option( 'show_title' ); ?>
+    <?php $show_tagline = photo_perfect_get_option( 'show_tagline' ); ?>
+    <?php if ( true == $show_title || true == $show_tagline ): ?>
+      <div id="site-identity">
+        <?php if ( true == $show_title ): ?>
+          <?php if ( is_front_page() && is_home() ) : ?>
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php else : ?>
+            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+          <?php endif; ?>
+        <?php endif ?>
+
+        <?php if ( true == $show_tagline ): ?>
+          <p class="site-description"><?php bloginfo( 'description' ); ?></p>
+        <?php endif ?>
+      </div><!-- #site-identity -->
+    <?php endif ?>
+
+		<?php do_action( 'photo_perfect_action_before' ); ?>
 	<?php
 	/**
 	 * Hook - photo_perfect_action_before_content.
